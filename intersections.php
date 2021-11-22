399e073bdab699d69171a9c2f29f0f92088c3321
nikolaibaranov1207 <94797018+nikolaibaranov1207@users.noreply.github.com>
2021-11-22 18:07:15 +0100

    Create intersections.php

---
+++ intersections.php
@@ -0,0 +1,17 @@
+<?php
+
+interface AInterface {...}
+interface BInterface {...}
+
+class A implements AInterface {...}
+class B implements BInterface {...}
+class AB implements AInterface, BInterface  {...}
+
+interface ABInterface extends AInterface, BInterface {}
+
+function function_for_A_and_B(ABInterface $value) {
+  ...
+}
+function function_for_A_or_B(AInterface|BInterface $value) {
+  ...
+}
